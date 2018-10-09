768ae91da06f346332fab97a3e04438ae1b19dd2
simon.zhang <simon.zhang@kilimall.com>
2018-10-09 21:58:40 +0800

    控制器。。

---
+++ app/Http/Controllers/Seller/ArticleController.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * 用于支付端的方法验证
+ */
+
+namespace App\Http\Controllers\Seller;
+
+
+use App\Models\Article;
+use App\Models\CostRecord;
+use App\Models\Freight;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use App\Seller\Facades\Seller;
+use Encore\Admin\Facades\Admin;
+use App\Seller\Layout\Content;
+use App\Repositories\FreightRepository;
+use Encore\Admin\Widgets\Box;
+use Encore\Admin\Widgets\Table;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+
+class ArticleController extends Controller
+{
+
+
+    public function index(){
+
+        return Seller::content(function (Content $content) {
+
+            $content->header(__('Article'));
+            $content->description(__('Article'));
+
+            $content->body($this->grid());
+
+        });
+    }
+
+    public function faq(){
+
+        return Seller::content(function (Content $content) {
+
+            $content->header(__('FAQ'));
+            $content->description(__('FAQ'));
+
+            $content->body($this->grid(1));
+
+        });
+    }
+
+    public function show($id){
+        //
+        return Seller::content( function (Content $content) use ($id) {
+
+            $article = Article::findOrFail($id);
+            $box = new Box($article->title, $article->content);
+            $content->row($box);
+//            echo $box;
+//            //运单的发货人信息
+//            $headers = ['Keys', 'Values'];
+//            $rows = ['SenderName2'=> '','SenderName1'=> '',
+//            ];
+//            $table = new Table($headers, $rows);
+//            $content->row((new Box('SenderInfo', $table))->style('info')->solid());
+
+
+        });
+    }
+
+
+    /**
+     * Make a grid builder.
+     * @param $type 类型
+     * @return Grid
+     */
+    protected function grid($type='')
+    {
+        return Seller::grid(Article::class, function (Grid $grid) use ($type){
+
+            $grid->model()->where('type_id',$type);
+            $grid->disableExport();
+            $grid->disableCreation();
+            $grid->disableRowSelector();
+            $grid->disableBatchDeletion();
+            $grid->disableActions();
+
+//            $grid->display('id', 'ID');
+//            $grid->title(__('Title'));
+            $grid->title()->value(function ($title) {
+                return "<a href='".url('/seller/article',[
+                    'id'=>$this->id
+                ])."'>$this->title</span></a>";
+            });
+
+
+            $grid->filter(function($filter) use ($type){
+
+            });
+
+        });
+    }
+
+
+}

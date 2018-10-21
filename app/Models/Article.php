340c9dc95a475abbf5a4828ef78ef82a3035f913
simon.zhang <simon.zhang@kilimall.com>
2018-10-21 23:06:37 +0800

    关系。。

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,13 +10,20 @@ class Article extends Model
     //table name
     protected $table = 'articles';
 
-    public static $categories = [
+    public static $types = [
         '0'=>'FAQ',
         '1'=>'视频',
         '2'=>'偷窥',
         '3'=>'文爱',
         '4'=>'手天使',
         '5'=>'公告',
+    ];
+
+    public static $user_types=[
+        '1'=>'视频',
+        '2'=>'偷窥',
+        '3'=>'文爱',
+        '4'=>'手天使',
     ];
 
 

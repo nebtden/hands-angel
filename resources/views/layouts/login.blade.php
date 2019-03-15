da60f4d7b96c6cb1a7080aab0beedb22d380e06b
simon.zhang <simon.zhang@kilimall.com>
2019-03-15 23:00:43 +0800

    模板。。

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,24 +5,21 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
             <div class="modal-body text-center clearfix">
-                <form class="form-login form-listing" action="index.html#" method="post"><h3
-                        class="title-formlogin">Log in</h3><span class="input-login icon-form"><input type="text"
-                                                                                                      placeholder="Your Name*"
-                                                                                                      name="email"
-                                                                                                      required="required"><i
-                            class="fa fa-user"></i></span><span class="input-login icon-form"><input type="text"
-                                                                                                     placeholder="Password*"
-                                                                                                     name="password"
-                                                                                                     required="required"><i
-                            class="fa fa-lock"></i></span>
-                    <div class="flat-fogot clearfix"><label class="float-left"><span class="input-check"><input
-                                    type="checkbox" id="rem" name="check" value="0" checked=""><label for="rem"
-                                                                                                      class="remember">Remember me</label></span></label><label
-                            class="float-right link-register"><a href="index.html#">Lost your password?</a></label>
+                <form class="form-login form-listing" action="{{ route('login') }}" method="post">
+                    @csrf
+                    <h3 class="title-formlogin">Log in</h3><span class="input-login icon-form">
+                        <input type="text" placeholder="Your Name*" name="email" required="required"><i class="fa fa-user"></i></span><span class="input-login icon-form">
+                        <input type="password" placeholder="Password*" name="password" required="required" value="{{ old('username') }}"><i class="fa fa-lock"></i></span>
+                    <div class="flat-fogot clearfix">
+                        <label class="float-left">
+                            <span class="input-check"><input type="checkbox" id="rem" name="check" value="0" checked="">
+                                <label for="rem" class="remember">Remember me</label></span></label><label class="float-right link-register">
+                            <a href="index.html#">Lost your password?</a>
+                        </label>
                     </div>
-                    <span class="wrap-button"><button type="button" id="login-button"
-                                                      class=" login-btn effect-button"
-                                                      title="log in">LOG IN</button></span></form>
+                    <span class="wrap-button">
+                        <button type="submit" id="login-button" class=" login-btn effect-button" title="log in">LOG IN</button></span>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -6,15 +6,11 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
             <div class="modal-body text-center clearfix">
-                <form class="form-login form-listing" action="index.html#" method="post"><h3
-                        class="title-formlogin">Sign Up</h3><span class="input-login icon-form"><input type="text"
-                                                                                                       placeholder="Your Name*"
-                                                                                                       name="name"
-                                                                                                       required="required"><i
-                            class="fa fa-user"></i></span><span class="input-login icon-form"><input type="text"
-                                                                                                     placeholder="E-mail*"
-                                                                                                     name="email"
-                                                                                                     required="required"><i
+                <form class="form-login form-listing" action="{{ route('register') }}" method="post">
+                    @csrf
+                    <h3 class="title-formlogin">Sign Up</h3>
+                    <span class="input-login icon-form">
+                        <input type="text" placeholder="Your Name*" name="name" required="required"><i class="fa fa-user"></i></span><span class="input-login icon-form"><input type="text" placeholder="E-mail*" name="email"required="required"><i
                             class="fa fa-envelope-o"></i></span><span class="input-login icon-form"><input type="text"
                                                                                                            placeholder="Password*"
                                                                                                            name="password"

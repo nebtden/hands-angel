dcaf107093318c9be523579e3fa79a75c358d3b9
simon <nebtden@gmail.com>
2019-03-19 19:49:19 +0800

    images

--- update.sql
+++ update.sql
@@ -1,8 +1,8 @@
 ALTER TABLE `users`
-    ADD COLUMN `mobile`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机号码' AFTER `country_id`;
+    ADD COLUMN `mobile`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机号码' ,
     ADD COLUMN `wechat`  varchar(255) NOT NULL AFTER `mobile`,
     ADD COLUMN `youtube`  varchar(255) NOT NULL AFTER `wechat`,
     ADD COLUMN `facebook`  varchar(255) NOT NULL AFTER `youtube`,
     ADD COLUMN `twitter`  varchar(255) NOT NULL AFTER `facebook`;
-    ADD COLUMN `head_img`  varchar(255) NOT NULL AFTER `name`;
+    ADD COLUMN `head_img`  varchar(255) NOT NULL AFTER `name`,
     ADD COLUMN `country_id`  tinyint(2) NOT NULL DEFAULT 0 COMMENT '国家id' AFTER `password_origin`;

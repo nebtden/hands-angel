1d3d5a18625a3d315456a48df63484c1722b4f8e
nebtden <nebtden@google.com>
2019-03-19 17:21:23 +0800

    信息更新。。

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,8 @@
 <link rel="stylesheet" type="text/css" href="/stylesheets/responsive.css"><!-- REVOLUTION LAYERS STYLES -->
 <link rel="stylesheet" type="text/css" href="/revolution/css/layers.css">
 <link rel="stylesheet" type="text/css" href="/revolution/css/settings.css"><!-- Animation Style -->
-<link rel="stylesheet" type="text/css" href="/stylesheets/animate.css"><!-- Favicon and touch icons  -->
+<link rel="stylesheet" type="text/css" href="/stylesheets/animate.css">
+<link rel="stylesheet" type="text/css" href="/stylesheets/my.css">
 <link href="/icon/apple-touch-icon-48-precomposed.png" rel="apple-touch-icon-precomposed" sizes="48x48">
 <link href="/icon/apple-touch-icon-32-precomposed.png" rel="apple-touch-icon-precomposed">
 <link href="/icon/favicon.png" rel="shortcut icon">
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -18,6 +18,8 @@
                 </div>
                 <div class="col-lg-9">
                     <div class="flat-tabs style2" data-effect="fadeIn">
+                        <form method="post" action="page-profile.html#" class="form-profile">
+                            {{csrf_token()}}}
                         <ul class="menu-tab clearfix">
                             <li class="active"><a href="page-profile.html#"><i class="ion-navicon-round"></i>(3)
                                     Listings</a></li>
@@ -31,21 +33,22 @@
                                         <div class="col-md-4">
                                             <div class="upload-img">
                                                 <div    class="dropzone" id="my-awesome-dropzone">
-                                                    {{ csrf_field() }}
+
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <form method="post" action="page-profile.html#" class="form-profile"><p class="input-info"><label>Your name*</label>
-                                                    {{ csrf_field() }}
-                                                    <input type="text" name="name" id="name"></p>
+
+                                                <p class="input-info"><label>Your name*</label>
+
+                                                    <input type="text" name="name" id="name" value="{{$user->name}}"></p>
                                                 <p class="input-info">
                                                     <label>Your Phone Numbers*</label>
-                                                    <input type="text" name="phone" id="phone"></p>
+                                                    <input type="text" name="phone" id="phone" value="{{$user->mobile}}"></p>
                                                 <p class="input-info">
                                                     <label>Your Phone Numbers*</label>
-                                                    <input type="text" name="email" id="email"></p>
-                                            </form>
+                                                    <input type="text" name="email" id="email">
+                                                </p>
                                         </div>
                                     </div>
                                 </div>
@@ -54,102 +57,39 @@
                                         <div class="col-md-4"><h5>On the web</h5></div>
                                         <div class="col-md-8">
                                             <ul class="add-section">
-                                                <li class="twitter"><i class="fa fa-twitter-square twitter"></i><span>Twitter</span><a href="https://twitter.com/" title="">https://twitter.com/</a>
-                                                    <i class="fa fa-minus-circle float-right"></i></li>
-                                                <li class="face"><i class="fa fa-facebook-square face"></i><span>Facebook</span>
-                                                    <a href="https://www.facebook.com/" title="">https://www.facebook.com/</a><i class="fa fa-minus-circle float-right"></i></li>
-                                                <li class="youtube"><i class="fa fa-youtube-square youtube"></i><span>Youtube</span><i class="fa fa-minus-circle float-right"></i></li>
-                                                <li class="add"><a href="page-profile.html#" class="add">Add new
-                                                        section</a><a href="page-profile.html#" title="" class="float-right"><i class="fa fa-plus-circle "></i></a>
+                                                <li class="twitter">
+                                                    <i class="fa fa-twitter-square twitter"></i>
+                                                    <span>Twitter</span>
+                                                    <a href="https://twitter.com/" title="">{{$user->twitter}}</a>
+                                                    <i class="fa fa-minus-circle float-right"></i>
                                                 </li>
+                                                <li class="face">
+                                                    <i class="fa fa-facebook-square face"></i>
+                                                    <span>Facebook</span>
+                                                    <a href="{{$user->facebook}}" title="">{{$user->facebook}}</a>
+                                                    <i class="fa fa-minus-circle float-right"></i>
+                                                </li>
+                                                <li class="weixin">
+                                                    <i class="fa fa-weixin weixin"></i>
+                                                    <span>Wechat</span>
+                                                    {{$user->wechat}}
+                                                    <i class="fa fa-minus-circle float-right"></i>
+                                                </li>
+
+
                                             </ul>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="update-button text-center">
-                                    <button type="button" class="flat-button" onclick="location.href='#'">Update
+                                    <button type="submit" class="flat-button"  >Update
                                         Profile
                                     </button>
                                 </div>
                             </div>
-                            <div class="content-inner">
-                                <div class="author-review content-listing">
-                                    <div class="comments-area">
-                                        <ol class="comment-list">
-                                            <li class="comment">
-                                                <article class="comment-body clearfix">
-                                                    <div class="comment-author">
-                                                        <img src="images/services/c1.png" alt="image"></div>
-                                                    <!-- .comment-author -->
-                                                    <div class="comment-text">
-                                                        <div class="comment-metadata"><h5><a href="page-profile.html#">Shaya Hill </a><span>on San Angelo Restaurants</span></h5>
-                                                            <p class="day">12/01/2017</p>
-                                                            <div class="flat-start"><i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star-half-o"></i>
-                                                            </div>
-                                                        </div><!-- .comment-metadata -->
-                                                        <div class="comment-content"><p>Lorem ipsum dolor sit amet,
-                                                                consectetur adipisicing elit, sed do eiusmod tempor
-                                                                incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                                minim veniam, quis nostrud exercitation ullamco</p></div>
-                                                        <!-- .comment-content -->
-                                                    </div><!-- /.comment-text -->
-                                                </article>
-                                                <!-- .comment-body -->
-                                            </li><!-- #comment-## -->
-                                            <li class="comment">
-                                                <article class="comment-body clearfix">
-                                                    <div class="comment-author"><img src="images/services/c2.png"
-                                                                                     alt="image"></div>
-                                                    <!-- .comment-author -->
-                                                    <div class="comment-text">
-                                                        <div class="comment-metadata"><h5><a href="page-profile.html#">Alex  Poole </a><span>on Grand Prairie Restaurants</span></h5>
-                                                            <p class="day">12/01/2017</p>
-                                                            <div class="flat-start">
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star"></i>
-                                                                <i class="fa fa-star-half-o"></i>
-                                                            </div>
-                                                        </div><!-- .comment-metadata -->
-                                                        <div class="comment-content"><p>Lorem ipsum dolor sit amet,
-                                                                consectetur adipisicing elit, sed do eiusmod tempor
-                                                                incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                                minim veniam, quis nostrud exercitation ullamco</p>
-                                                        </div>
-                                                        <!-- .comment-content -->
-                                                    </div><!-- /.comment-text -->
-                                                </article>
-                                                <!-- .comment-body -->
-                                            </li><!-- #comment-## -->
-                                            <li class="comment">
-                                                <article class="comment-body clearfix">
-                                                    <div class="comment-author"><img src="images/services/c3.png"
-                                                                                     alt="image"></div>
-                                                    <!-- .comment-author -->
-                                                    <div class="comment-text">
-                                                        <div class="comment-metadata"><h5><a href="page-profile.html#">Anthony
-                                                                    Jones </a><span>on San Antonio Restaurants</span></h5>
-                                                            <p class="day">12/01/2017</p>
-                                                            <div class="flat-start"><i class="fa fa-star"></i><i
-                                                                    class="fa fa-star"></i><i class="fa fa-star"></i><i
-                                                                    class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
-                                                            </div>
-                                                        </div><!-- .comment-metadata -->
-                                                        <div class="comment-content"><p>Lorem ipsum dolor sit amet,
-                                                                consectetur adipisicing elit, sed do eiusmod tempor
-                                                                incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                                                minim veniam, quis nostrud exercitation ullamco</p></div>
-                                                        <!-- .comment-content --></div><!-- /.comment-text --></article>
-                                                <!-- .comment-body --></li><!-- #comment-## --></ol>
-                                        <!-- .comment-list --></div>
-                                </div>
-                            </div>
+
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>

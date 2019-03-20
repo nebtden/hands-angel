328173722db0c88b4a2c2c21e93f4f42477673ec
nebtden <nebtden@google.com>
2019-03-20 16:15:40 +0800

    register.blade.php

--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -1,4 +1,3 @@
-
 <div class="modal fade flat-popupform" id="popup_register">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -9,40 +8,35 @@
                 <form class="form-login form-listing" action="{{ route('register') }}" method="post">
                     @csrf
                     <h3 class="title-formlogin">Sign Up</h3>
-                    <span class="input-login icon-form">
-                        <input type="text" placeholder="Your Name*" name="name" required="required">
-                        <i class="fa fa-user"></i>
-                    </span>
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                    @endif
-                    <span class="input-login icon-form">
-                        <input type="text" placeholder="E-mail*" name="email"required="required">
-                        <i class="fa fa-envelope-o"></i>
-                    </span>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                    @endif
+                    <div class="form-group has-feedback">
+                        <span class="input-login icon-form">
+                            <input type="text" placeholder="Your Name*" name="name" required="required">
+                            <i class="fa fa-user"></i>
+                        </span>
+                    </div>
 
-                    <span class="input-login icon-form">
-                        <input type="text" placeholder="Password*"  name="password" required="required">
-                        <i class="fa fa-lock"></i>
-                    </span>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                    @endif
-                    <span class="input-login icon-form">
-                        <input type="text" placeholder="Repeat Password*" name="password_origin" required="required">
-                        <i class="fa fa-lock"></i>
-                    </span>
+                    <div class="form-group has-feedback">
+                        <span class="input-login icon-form">
+                            <input type="text" placeholder="E-mail*" name="email" required="required">
+                            <i class="fa fa-envelope-o"></i>
+                        </span>
+                    </div>
+
+                    <div class="form-group has-feedback">
+                        <span class="input-login icon-form">
+                            <input type="text" placeholder="Password*" name="password" required="required">
+                              <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
+
+                    <div class="form-group has-feedback">
+                        <span class="input-login icon-form">
+                            <input type="text" placeholder="Repeat Password*" name="password_origin" required="required">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
                     <div class="wrap-button signup">
-                        <button type="submit" id="logup-button" class=" login-btn effect-button" title="log in">LOG  UP
+                        <button type="submit" id="logup-button" class=" login-btn effect-button" title="log in">LOG UP
                         </button>
                     </div>
                 </form>
@@ -50,4 +44,49 @@
         </div>
     </div>
 </div>
-<!-- Go Top --><a class="go-top effect-button"><i class="fa fa-angle-up"></i></a>
+
+@section('javascript')
+    <script type="application/javascript">
+        $(function () {
+
+            $(document).on('submit', '#popup_register', function (e) {
+                e.preventDefault();
+
+                $('input+span>strong').text('');
+                $('input').parent().parent().removeClass('has-error');
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    type: 'POST',
+                    url: "{{ route('login') }}",
+                    data: {
+                        "name": $('#popup_register').find("input[name=name]").val(),
+                        "email": $('#popup_register').find("input[name=email]").val(),
+                        "password": $('#popup_register').find("input[name=password]").val(),
+                        "password_origin": $('#popup_register').find("input[name=password_origin]").val(),
+                    },
+                    beforeSend: function () {
+                        $('#popup_login .has-error').remove()
+                    },
+                    success: function (data) {
+                        // $("#result").html(data.success);
+                        location.href = '/test'
+                    },
+                    error: function (request, status, error) {
+                        json = $.parseJSON(request.responseText);
+                        $.each(json.errors, function (key, value) {
+                            $('#popup_login').find("input[name=" + key + "]").parent().parent().prepend('<label class="control-label has-error" for="' + key + '"> ' + value[0] + '</label></br>');
+                        });
+                    }
+                });
+            });
+        });
+    </script>
+
+@endsection()
